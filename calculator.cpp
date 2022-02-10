2d3bf4fd903fe1f51be30980e866cedefa90d97b
Zach <56052062+zgould0308@users.noreply.github.com>
2022-02-10 14:03:37 -0500

    Adding other math functions
    Changes:
    -4 new math functions
    -added the solving/solution printing code to the the corresponding function
    -added a do/while loop that let's the client do multiple equations without exiting the script

--- calculator.cpp
+++ calculator.cpp
@@ -1,25 +1,107 @@
 #include <iostream>
+#include <cmath>
 
-class Input{
+class InOut{
     public:
-        double dvd;
-        double dvs;
+        double in1;
+        double in2;
+        double ans;
         
-        void setEquation(){
+        void Addition(){
+            std::cout<<"Input augend:\n";
+            std::cin>>in1;
+            std::cout<<"Input addend:\n";
+            std::cin>>in2;
+            ans = in1 + in2;
+            std::cout<<"The sum is: "<<ans<<"\n";
+        }
+        void Subtraction(){
+            std::cout<<"Input minuend:\n";
+            std::cin>>in1;
+            std::cout<<"Input subtrahend:\n";
+            std::cin>>in2;
+            ans = in1 - in2;
+            std::cout<<"The difference is: "<<ans<<"\n";
+        }
+        void Multiplication(){
+            std::cout<<"Input multiplicand:\n";
+            std::cin>>in1;
+            std::cout<<"Input multiplier:\n";
+            std::cin>>in2;
+            ans = in1 * in2;
+            std::cout<<"The product is: "<<ans<<"\n";
+        }
+        void Division(){
             std::cout<<"Input dividend:\n";
-            std::cin>>dvd;
+            std::cin>>in1;
             std::cout<<"Input divisor:\n";
-            std::cin>>dvs;
+            std::cin>>in2;
+            ans = in1 / in2;
+            std::cout<<"The quotient is: "<<ans<<"\n";
+        }
+        void Exponentiation(){
+            std::cout<<"Input base:\n";
+            std::cin>>in1;
+            std::cout<<"Input exponent:\n";
+            std::cin>>in2;
+            ans = pow(in1,in2);
+            std::cout<<"The product is: "<<ans<<"\n";
         }
 };
 
 int main()
-{
-    double quotient;
-    Input In;
+{   
+    InOut inout;
     
-    In.setEquation();
-    quotient = In.dvd/In.dvs;
-    std::cout<<"The quotient is: "<<quotient<<std::endl;
+    bool playing {true};
+    int playagain;
+    unsigned int equ;
+    
+    do{
+        
+        std::cout<<"Select equation type:\n1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Exponentiation\n";
+        std::cin>>equ;
+        
+        if(equ >= 1 && equ <= 5){
+            switch(equ){
+                case 1:
+                    inout.Addition();
+                    break;
+                case 2:
+                    inout.Subtraction();
+                    break;
+                case 3:
+                    inout.Multiplication();
+                    break;
+                case 4:
+                    inout.Division();
+                    break;
+                case 5:
+                    inout.Exponentiation();
+            }
+            
+            std::cout<<"Would you like me to solve another equation?\n1) Yes\n2) No\n";
+            std::cin>>playagain;
+            
+            if(playagain >= 1 && playagain <= 2){
+                switch(playagain){
+                    case 1:
+                        playing = true;
+                        break;
+                    case 2:
+                        playing = false;
+                        break;
+                }
+            }
+            else{
+            std::cout<<"Invalid choice. Please try again.\n";
+            continue;
+            }
+        }
+        else{
+            std::cout<<"Invalid choice. Please try again.\n";
+            continue;
+        }
+    }while(playing == true);
     return 0;
 }
